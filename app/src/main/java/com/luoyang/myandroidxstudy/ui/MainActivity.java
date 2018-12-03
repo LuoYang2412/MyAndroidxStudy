b26d53a87bf15d7937985aaf1146b399a5bed2a3
LuoYang <toluoyang@qq.com>
2018-12-03 11:22:29 +0800

    增加首页java代码

--- app/src/main/java/com/luoyang/myandroidxstudy/ui/MainActivity.java
+++ app/src/main/java/com/luoyang/myandroidxstudy/ui/MainActivity.java
@@ -1,18 +1,93 @@
 package com.luoyang.myandroidxstudy.ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.luoyang.myandroidxstudy.R;
+import com.luoyang.myandroidxstudy.util.ToastUtil;
 
-public class MainActivity extends AppCompatActivity {
-    private NavHostFragment currentFragment;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private NavHostFragment currentNavHostFragment;
+    private NavHostFragment findFragmentHost;
+    private NavHostFragment boughtFragmentHost;
+    private NavHostFragment meFragmentHost;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        findFragmentHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHost_fragment_find);
+        boughtFragmentHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHost_fragment_bought);
+        meFragmentHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHost_fragment_me);
+        getSupportFragmentManager().beginTransaction().show(findFragmentHost).hide(boughtFragmentHost).hide(meFragmentHost).commit();
+        currentNavHostFragment = findFragmentHost;
+
+        currentNavHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHost_fragment_find);
+        if (currentNavHostFragment != null) {
+            NavigationUI.setupWithNavController(toolbar, currentNavHostFragment.getNavController());
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.hide(currentNavHostFragment);
+        switch (item.getItemId()) {
+            case R.id.navigation_find:
+                fragmentTransaction.show(findFragmentHost).commit();
+                currentNavHostFragment = findFragmentHost;
+                NavigationUI.setupWithNavController(toolbar, currentNavHostFragment.getNavController());
+                invalidateOptionsMenu();
+                return true;
+            case R.id.navigation_bought:
+                fragmentTransaction.show(boughtFragmentHost).commit();
+                currentNavHostFragment = boughtFragmentHost;
+                NavigationUI.setupWithNavController(toolbar, currentNavHostFragment.getNavController());
+                invalidateOptionsMenu();
+                return true;
+            case R.id.navigation_me:
+                fragmentTransaction.show(meFragmentHost).commit();
+                currentNavHostFragment = meFragmentHost;
+                NavigationUI.setupWithNavController(toolbar, currentNavHostFragment.getNavController());
+                invalidateOptionsMenu();
+                return true;
+            default:
+        }
+        return false;
+    }
+
+    private long oneTime = 0L;
+    private long exitTime = 1000;
+
+    @Override
+    public void onBackPressed() {
+        Fragment currentFragment = currentNavHostFragment.getChildFragmentManager().getPrimaryNavigationFragment();
+        if (currentFragment instanceof FindFragment || currentFragment instanceof BoughtFragment || currentFragment instanceof MeFragment) {
+            long towTime = System.currentTimeMillis();
+            if (towTime - oneTime < exitTime) {
+                super.onBackPressed();
+            } else {
+                ToastUtil.INSTANCE.show("再按一次退出");
+                oneTime = towTime;
+            }
+        } else {
+            currentNavHostFragment.getNavController().navigateUp();
+        }
     }
 }
